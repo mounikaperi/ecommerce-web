649eaa159cb87383e9c65acb030d4e1357bf4e53
mounikaperi <saimounikaperi@gmail.com>
2025-01-01 12:12:43 +0530

    Starting with App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import WebFont from 'webfontloader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const dispatch = useDispatch();
+  const { pathName } = useLocation;
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto:300,400,500,600,700"]
+      }
+    });
+  });
+
+  // always scroll to top on route/path change
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+  }, [pathName])
+
+  // disable right click
+  window.addEventListener("contextmenu", (e) => e.preventDefault());
+  window.addEventListener("keydown", (e) => {
+    if (e.keyCode === 123) e.preventDefault(); //  Opens the browser's developer tools.
+    if (e.ctrlKey && e.shiftKey && e.keyCode === 73) e.preventDefault(); // Opens the "Elements" panel in developer tools.
+    if (e.ctrlKey && e.shiftKey && e.keyCode === 74) e.preventDefault(); // Opens the "Console" in developer tools.
+  })
 }
 
 export default App;
