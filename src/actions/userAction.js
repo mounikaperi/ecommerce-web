89264f4559c9b2f32dba1eedbab49608c8b27f62
mounikaperi <saimounikaperi@gmail.com>
2025-01-04 09:57:11 +0530

    Action and url configs

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { getLoginUserUrl, getLogoutUserUrl, getRegisterUserUrl } from '../utils/urlConfig';
-import { LOGIN_USER, LOGOUT_USER, REGISTER_USER } from '../constants/userConstants';
+import { getLoadUserUrl, getLoginUserUrl, getLogoutUserUrl, getRegisterUserUrl, getUpdateUserPasswordUrl, getUpdateUserProfileUrl } from '../utils/urlConfig';
+import { LOAD_USER, LOGIN_USER, LOGOUT_USER, REGISTER_USER, UPDATE_PASSWORD, UPDATE_PROFILE } from '../constants/userConstants';
 
 const registerUser = (userData) => async (dispatch) => {
   try {
@@ -56,8 +56,74 @@ const logoutUser = () => async (dispatch) => {
   }
 };
 
+const loadUser = () => async (dispatch) => {
+  try {
+    dispatch({ type: LOAD_USER.LOAD_USER_REQUEST });
+    const { data } = await axios.get(getLoadUserUrl());
+    dispatch({ type: LOAD_USER.LOAD_USER_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({
+      type: LOAD_USER.LOAD_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+const updateProfile = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_PROFILE.UPDATE_PROFILE_REQUEST });
+    const config = {
+      headers: {
+          "Content-Type": "multipart/form-data",
+      }
+    };
+    const { data } = await axios.put(
+      getUpdateUserProfileUrl(),
+      userData,
+      config
+    );
+    dispatch({
+      type: UPDATE_PROFILE.UPDATE_PROFILE_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PROFILE.UPDATE_PROFILE_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+ 
+const updatePassword = (passwords) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_PASSWORD.UPDATE_PASSWORD_REQUEST });
+    const config = {
+      headers: {
+          "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.put(
+      getUpdateUserPasswordUrl(),
+      passwords,
+      config
+    );
+    dispatch({
+      type: UPDATE_PASSWORD.UPDATE_PASSWORD_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PASSWORD.UPDATE_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
 module.exports = {
   registerUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  loadUser,
+  updateProfile,
+  updatePassword
 }
--- src/utils/urlConfig.js
+++ src/utils/urlConfig.js
@@ -2,14 +2,29 @@ const getAllProducts = () => '/api/v1/products/all';
 const getProduct = (id) => `/api/v1/product/${id}`;
 const getProductsUrl = () => '/api/v1/products';
 
+const getRegisterUserUrl = () => '/api/v1/register';
 const getLogoutUserUrl = () => '/api/v1/logout';
 const getLoginUserUrl = () => '/api/v1/login';
-
-const getRegisterUserUrl = () => '/api/v1/register';
+const getLoadUserUrl = () => '/api/v1/me';
+const getUpdateUserProfileUrl = () => '/api/v1/me/update';
+const getUpdateUserPasswordUrl = () => '/api/v1/password/update';
 
 const getNewReviewUserUrl = () => '/api/v1/review';
 
 const getAddItemToCartUrl = (id) => `/api/v1/product/${id}`;
+
+const getPaymentProcessUrl = () => '/api/v1/payment/process';
+const getPaytmProcessUrl = () => 'https://securegw-stage.paytm.in/order/process';
+const getPaymentStatusUrl = (id) => `/api/v1/payment/status/${id}`;
+
+const getNewOrderUrl = () => '/api/v1/order/new';
+const getMyOrdersUrl = () => '/api/v1/orders/me';
+const getAllOrdersUrl = () => '/api/v1/admin/orders';
+const getUpdateOrderUrl = (id) => `/api/v1/admin/order/${id}`;
+const getDeleteOrderUrl = (id) => `/api/v1/admin/order/${id}`;
+const getOrderDetailsUrl = (id) => `/api/v1/order/${id}`;
+
+
 
 module.exports = {
   getAllProducts,
@@ -19,5 +34,17 @@ module.exports = {
   getLogoutUserUrl,
   getRegisterUserUrl,
   getNewReviewUserUrl,
-  getAddItemToCartUrl
+  getAddItemToCartUrl,
+  getPaymentProcessUrl,
+  getPaytmProcessUrl,
+  getNewOrderUrl,
+  getMyOrdersUrl,
+  getOrderDetailsUrl,
+  getPaymentStatusUrl,
+  getAllOrdersUrl,
+  getUpdateOrderUrl,
+  getDeleteOrderUrl,
+  getLoadUserUrl,
+  getUpdateUserProfileUrl,
+  getUpdateUserPasswordUrl
 }
